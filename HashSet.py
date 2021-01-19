874f23f12b020d6098009c6da5c9111d615767fc
Caoimhe De Buitlear <57628459+debuitc4@users.noreply.github.com>
2021-01-19 13:46:09 +0000

    Add files via upload

---
+++ HashSet.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import sys
+
+class Node:
+    def __init__(self, item, next):
+        self.item = item
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+        self.head = Node(item, self.head)
+
+    def remove(self):
+        if self.is_empty():
+            return None
+        else:
+            item = self.head.item
+            self.head = self.head.next    # remove the item by moving the head pointer
+            return item
+
+    def is_empty(self):
+        return self.head == None
+
+    def __iter__(self):
+        cursor = self.head
+        while cursor is not None:
+            yield cursor.item
+            cursor = cursor.next
+
+    def recursive_len(self, ptr):
+        if ptr == None:
+            return 0
+        else:
+            return 1 + self.recursive_len(ptr.next)
+
+    def __len__(self):
+        return self.recursive_len(self.head)
+
+    def recursive_contains(self, ptr, item):
+        if ptr == None:
+            return False
+        else:
+            return item == ptr.item or self.recursive_contains(ptr.next)
+
+    def __in__(self, item):
+        return recursive_contains(self.head, item)
+
+    def recursive_str(self, ptr):
+        if ptr == None:
+            return ""
+        else:
+            return str(ptr.item) + "->" + self.recursive_str(ptr.next)
+
+    def __str__(self):
+        return self.recursive_str(self.head)
+
+class HashSet:
+    def __init__(self, capacity=10):
+        # Create a list to use as the hash table
+        self.table = [None] * capacity
+
+    def add(self, item):
+        # Find the hash code
+        h = hash(item)
+        index = h % len(self.table)
+
+        # Check is it empty
+        if self.table[index] == None:
+            self.table[index] = LinkedList() # Need a new linked list for this entry
+
+        if item not in self.table[index]:
+            # Only add it if not already there (this is a set)
+            self.table[index].add(item)
+        
+        #if there is no collison ex: no item
+        #in the linked list after the first
+        if self.table[index].head.next == None:
+            return None
+        else:
+            return index, h
+
+
+def main():
+    # Read each test case
+    line = sys.stdin.readline()
+    items = line.strip().split()
+    nums = [int(item) for item in items]
+    
+    # First number is the capacity
+    numset = HashSet(nums[0])
+
+    for x in nums[1:]:
+        print(str(numset.add(x)) + " ", end="")
+
+    print()
+
+if __name__ == "__main__":
+    main()
